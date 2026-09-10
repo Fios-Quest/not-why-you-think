faf3acf91703929f3790b1d377658e5b9187839e
Daniel Mason <daniel@danielmason.com>
2026-09-11 00:10:23 +0100

    feat: ssg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+use std::error::Error;
 use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
-fn main() {
-    dioxus::launch(App);
+fn main() -> Result<(), Box<dyn Error>> {
+    LaunchBuilder::new()
+        // Set the server config only if we are building the server target
+        .with_cfg(server_only! {
+            ServeConfig::builder()
+                // Enable incremental rendering
+                .incremental(
+                    dioxus::server::IncrementalRendererConfig::new()
+                        // Store static files in the public directory where other static assets like wasm are stored
+                        .static_dir(
+                            std::env::current_exe()?
+                                .parent()?
+                                .join("public")
+                        )
+                        // Don't clear the public folder on every build. The public folder has other files including the wasm
+                        // binary and static assets required for the app to run
+                        .clear_cache(false)
+                )
+                .enable_out_of_order_streaming()
+        })
+        .launch(App);
+    Ok(())
 }
 
 #[component]
